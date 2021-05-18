893c316ca256a8df2f97f12cb4b53c0605d67a31
nik0811 <nikhil811raj@gmail.com>
2021-05-18 11:59:27 -0700

    refresh token url added

--- api/metricsviewsapi/users/urls.py
+++ api/metricsviewsapi/users/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
 app_name = 'users-api'
 
 urlpatterns = [
 
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.MyTokenObtainPairView.as_view(), name='login'),
+    path('refresh_token/', TokenRefreshView.as_view(), name='refresh_token'),
+
 
     path('profile_update/', views.UserProfileUpdate.as_view(), name="profile_update"), 
     path('profile_update/photo/', views.ProfilePictureUpdate.as_view(), name="profile_update_photo"), 
